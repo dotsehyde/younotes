68fd8bd09e0db9e66cb1dce50f716280250fe0e0
Benjamin <dotsehyde@gmail.com>
2021-08-04 10:05:06 +0000

    update

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 export default function Notes() {
   return (
     <div>
-      Notes page
+      <Typography variant="h1" color="error">
+        Notes page
+      </Typography>
     </div>
-  )
+  );
 }
