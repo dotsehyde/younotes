51b3b8ea4cc9bc503df2c1dc95d349c0bcf4b4c7
Benjamin <dotsehyde@gmail.com>
2021-12-09 09:09:41 +0000

    Components

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="app-wrapper">
+    <div class="app">
+      <Navigation />
+      <router-view />
+      <Footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from "../src/components/Navigation.vue";
+import Footer from './components/Footer.vue';
+export default {
+  name: "app",
+  components: {
+    Navigation,
+    Footer,
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap");
+:root {
+  --primaryColor: #28223a;
+  --accentColor: #f3b643;
+  --whiteColor: #f6f9f8;
+  --greyColor: #848284;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Outfit", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.link {
+  cursor: pointer;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: black;
+}
+
+.link-light {
+  color: #fff;
+}
+</style>
